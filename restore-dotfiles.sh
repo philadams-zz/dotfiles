23d69421e5c974b2ee7db935052335c823630ff8
Phil Adams <phil@philadams.net>
2012-04-05 10:57:35 -0400

    make bash tab complete case insensitive

--- restore-dotfiles.sh
+++ restore-dotfiles.sh
@@ -4,6 +4,7 @@ CWD=`pwd`
 
 ln -snf $CWD/profile ~/.profile
 ln -snf $CWD/vimrc ~/.vimrc
+ln -snf $CWD/inputrc ~/.inputrc
 ln -snf $CWD/vim ~/.vim
 # ln -snf $CWD/vim ~/.vim
 ln -snf $CWD/gitconfig ~/.gitconfig
