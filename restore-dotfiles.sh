45f61079e9bbf68f0483ec5aade966d25da6e564
Phil Adams <phil@philadams.net>
2012-07-13 11:14:55 -0400

    os x, python, latex for global gitignore

--- restore-dotfiles.sh
+++ restore-dotfiles.sh
@@ -7,6 +7,7 @@ ln -snf $CWD/profile ~/.profile
 ln -snf $CWD/inputrc ~/.inputrc
 
 ln -snf $CWD/gitconfig ~/.gitconfig
+ln -snf $CWD/gitignore-global ~/.gitignore-global
 
 # now using janus for vim config (https://github.com/carlhuda/janus)
 # and tracking .vimrc.before, .vimrc.after, and .janus/
